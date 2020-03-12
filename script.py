49e0733fcc2eb80359048d79a3890b322742101c
Ramy Ibrahim <ramy_ibrahim99@hotmail.com>
2020-03-12 07:14:03 +0200

    Added: Print Student name and email.

--- script.py
+++ script.py
@@ -3,3 +3,9 @@ Created by: Ramy Ibrahim
 Date: 12 March 2020
 Supervised by: Dr. Younes Gamal Elddin
 '''
+
+student_name = 'Ramy Ibrahim'
+student_id = '20201000071'
+
+print('Name:', student_name)
+print('ID:', student_id)
